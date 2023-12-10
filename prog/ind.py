6e30bdebc4ea5a85bf191695a7412251bf4ae1fa
FiaLDI <leon.youtube2017@yandex.ru>
2023-12-10 21:48:59 +0300

    code

--- prog/ind.py
+++ prog/ind.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-def inl(func):
+
+def print_perimetr(func):
     def wrapper(*args, **kwars):
         return_value = func(*args, **kwars)
         print('Периметр фигуры равен', return_value)
     
     return wrapper
 
-@inl
+
+@print_perimetr
 def periment(*args):
     values = [float(arg) for arg in args]
     P = 0
@@ -15,8 +19,10 @@ def periment(*args):
         P += arg
     return P
 
+
 def main():
     periment(1, 2, 3, 4)
+
 
 if __name__ == "__main__":
     main()
